10486ce07eebf22c7aa5c270488993f34e551a7e
Maxim Fedorov <maxik64@gmail.com>
2022-12-18 11:51:32 +0300

    last commit before trying to deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const Modal = ({ open, setOpen }) => (
         <title/>
         <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/>
       </svg>
-      <img src="https://media.tenor.com/BfGFYekoftQAAAAC/spongebob-squarepants-spongebob.gif"/>
+      <img src="https://media.tenor.com/BfGFYekoftQAAAAC/spongebob-squarepants-spongebob.gif" alt=''/>
     </div>
   </div>
 )
